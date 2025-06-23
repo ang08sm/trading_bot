eea41455cd71f3c99a43bfecf6acdf8cf419bdc3
ang08sm <angsm008@gmail.com>
2025-06-23 12:52:05 +0530

    Logging configuration

--- logging_config.py
+++ logging_config.py
@@ -21,3 +21,18 @@ def setup_logging() :
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
+
+    file_handler = RotatingFileHandler(
+        log_file_path,
+        maxBytes=5 * 1024 * 1024,
+        backupCount=5
+    )
+
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    logging.getLogger('binance.client').setLevel(logging.WARNING)
+    logging.getLogger('urllib3').setLevel(logging.WARNING)
+
+    logger.info("Logging configured successfully")
